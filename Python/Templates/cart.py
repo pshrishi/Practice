83f9ffccca59205c9ba580501a8eb663e79aa33e
Girish Patil <ps.hrishi.reg@gmail.com>
2017-08-30 17:28:35 -0700

    Templates

---
+++ Python/Templates/cart.py
@@ -0,0 +1,19 @@
+from string import Template
+
+def Main():
+	cart = []
+	cart.append(dict(name="Coke", price=10, qty=3))
+	cart.append(dict(name="Pepsi", price=5, qty=2))
+	cart.append(dict(name="Sierra", price=15, qty=4))
+
+	template = Template("$qty x $name = $price")
+
+	total = 0
+	for item in cart:
+		print(template.safe_substitute(item))
+		total += item["price"]
+
+	print("Total: " + str(total))
+
+if __name__ == '__main__':
+	Main()
